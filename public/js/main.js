87e762e9c79402e30b8b8e06876dd07db3e6d6cd
AyobamiH <heryourbarme@live.com>
2022-08-21 21:07:14 +0100

    added pomodoro feature and improvised all crud operations

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,19 @@
+// Variables
+
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
+const startBtn = document.querySelector('#start')
+const sessionValue = document.querySelectorAll('.sessionValue')
+const reset = document.querySelector('#reset')
+
+let isRunning = false, count = 0, startTime = 0 , currentTime = 0, elapsedTime = 0, stopTime, 
+totalMinute,clockInterval, hours, remainder = 0, minutes = 0, seconds = 0, formattedTime = 0;
+
+// Event Listeners
+reset.addEventListener('click', reSet)
+startBtn.addEventListener('click', twentyFiveMinutesTimer)
+// document.querySelector('p').addEventListener('onchange', fiveMinutesBreakTimer)
 
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
@@ -14,8 +27,14 @@ Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+
+
+// Request Data from API
+
+// A Delete request
 async function deleteItem(){
     const itemText = this.parentNode.childNodes[1].innerText
+    console.log(itemText)
     try{
         const response = await fetch('deleteItem', {
             method: 'delete',
@@ -25,7 +44,6 @@ async function deleteItem(){
             })
           })
         const data = await response.json()
-        console.log(data)
         location.reload()
 
     }catch(err){
@@ -33,14 +51,48 @@ async function deleteItem(){
     }
 }
 
+
+// An Update request
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
+  
+    const totalPomoCount = document.querySelector('.sessionValue')
+               totalPomoCount.innerText = count
+
     try{
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                'itemFromJS': itemText
+                'itemFromJS': itemText,
+               'pomoValue': totalPomoCount.innerText
+               
+
+            })
+          })
+          
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+// Update request marking todo list uncomplete
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    const totalPomoCount = document.querySelector('.sessionValue').innerText
+    
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText,
+                'pomoValue': totalPomoCount
             })
           })
         const data = await response.json()
@@ -52,21 +104,168 @@ async function markComplete(){
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+// Leading zero
+function add_leading_zero(number){
+    if(number < 10) {
+        return "0" + number.toString();
+    } else {
+        return number.toString();
+    }
+}
 
-    }catch(err){
-        console.log(err)
+//....... Pomodoro Timers......//
+
+// ---- Twenty five minutes
+
+function twentyFiveMinutesTimer(){
+    if (!isRunning) {
+        // start the clock
+        isRunning = true;
+
+        if (elapsedTime == 0) {
+            startTime = new Date().getTime();
+
+        } else {
+            startTime = new Date().getTime() - elapsedTime;
+            console.log(startTime)
+        }
+
+        clockInterval = window.setInterval(function(){
+
+            currentTime = new Date().getTime();
+            elapsedTime = currentTime - startTime;
+            
+            // milliseconds per hour is 3600000
+            hours = Math.floor(elapsedTime / 3600000);
+            remainder = elapsedTime - (hours * 3600000);
+
+            // milliseconds per minute is 60000
+            minutes = Math.floor(remainder / 60000);
+            remainder -= (minutes * 60000);
+
+            // milliseconds per second is 1000
+            seconds = Math.floor(remainder / 1000);
+            remainder -= (seconds * 1000);
+
+            let formattedMinutes = parseInt(minutes)
+            let formattedSeconds = parseInt(seconds)
+            
+            if( formattedMinutes == 25 ){
+                                               
+                // announce the end of pomodoro
+                count += 1
+               
+                //Insert Values into the dom
+                document.querySelector('p').innerHTML = 'BREAK'
+                document.querySelector('.sessionValue').innerHTML = counts
+                document.querySelector('.countdownSeconds').innerHTML = add_leading_zero(formattedSeconds)
+                document.querySelector('.countdownMinutes').innerHTML = add_leading_zero(formattedMinutes)
+
+                // Clear the clock interval
+                window.clearInterval(clockInterval)
+
+                //Start Five Minutes break after every pomodoro session ends
+                fiveMinutesBreakTimer()
+                elapsedTime = 0
+                isRunning = false
+                                
+            }else{
+                document.querySelector('.countdownSeconds').innerHTML = add_leading_zero(formattedSeconds)
+                document.querySelector('.countdownMinutes').innerHTML = add_leading_zero(formattedMinutes)
+                document.querySelector('.sessionValue').innerHTML = counts
+            }
+            
+        },1000);
+
+    } else {
+
+        // stop the clock
+        window.clearInterval(clockInterval);
+        isRunning = false;
+
+    }
+}
+
+// Variables
+
+let running = false, counts = 0, startBreakTime = 0 , currentTimeonBreak = 0, elapsedBreakTime = 0, stopTimeBreak, 
+clockIntervalBreak, hoursBreak, remainderBreak = 0, minutesBreak = 0, secondsBreak = 0, formattedTimeBreak = 0;
+
+// Five minutes functions
+
+function fiveMinutesBreakTimer(){
+    if (!running) {
+        // start the clock
+        running = true;
+
+        if (elapsedBreakTime == 0) {
+            startBreakTime = new Date().getTime();
+
+        } else {
+            startBreakTime = new Date().getTime() - elapsedBreakTime;
+            console.log(startBreakTime)
+        }
+
+        clockIntervalBreak = window.setInterval(function(){
+
+            currentTimeonBreak = new Date().getTime();
+            elapsedBreakTime = currentTimeonBreak - startBreakTime;
+
+            // per hour 3600000
+            // per minute 60000
+            // per second 1000
+            hoursBreak = Math.floor(elapsedBreakTime / 3600000);
+            remainderBreak = elapsedBreakTime - (hoursBreak * 3600000);
+
+            minutesBreak = Math.floor(remainderBreak / 60000);
+            remainderBreak -= (minutesBreak * 60000);
+
+            secondsBreak = Math.floor(remainderBreak / 1000);
+            remainderBreak -= (secondsBreak * 1000);
+
+            
+            let formattedBreakMinutes = parseInt(minutesBreak)
+            let formattedBreakSeconds = parseInt(secondsBreak)
+            // console.log(formattedBreakSeconds)
+
+            if( formattedBreakMinutes == 5 ){
+               
+                counts += 1
+               document.querySelector('.sessionValue').innerHTML = counts
+               document.querySelector('.countdownSeconds').innerHTML = add_leading_zero(formattedBreakSeconds)
+                document.querySelector('.countdownMinutes').innerHTML = add_leading_zero(formattedBreakMinutes)
+                 // announce the end of break
+                document.querySelector('p').innerHTML = 'BREAK OVER'
+               
+                // clear the time interval
+                elapsedBreakTime = 0
+                window.clearInterval(clockIntervalBreak)
+                running = false
+                
+                
+            }else{
+                document.querySelector('.sessionValue').innerHTML = counts
+                document.querySelector('.countdownSeconds').innerHTML = add_leading_zero(formattedBreakSeconds)
+                document.querySelector('.countdownMinutes').innerHTML = add_leading_zero(formattedBreakMinutes)
+            
+            }
+            
+        },1000);
+
+    } else {
+        // stop the clock
+        window.clearInterval(clockIntervalBreak);
+        running = false;
+    }
+}
+
+// Timer reset
+function reSet() {
+
+    startTime = new Date().getTime();
+    if (!running && !isRunning) {
+        elapsedTime = 0;
+        document.querySelector('.countdownMinutes').innerHTML = "00";
+        document.querySelector('.countdownSeconds').innerHTML = "00"
     }
 }
--- server.js
+++ server.js
@@ -1,59 +1,74 @@
-//
+// Require all modules
+
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
+const store = require("store2");
 require('dotenv').config()
 
-// Variables
+
+
+// declare, initialise variable 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-// Making a connection to MongoDB 
+    // Connect to the database using mongoclient
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
 
-// Setting apps view engine to ejs    
-app.set('view engine', 'ejs')
 
+    
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-//Get method route
+// Setting up routes for CRUD operation
+
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const itemscompleted = await db.collection('todos').countDocuments({completed: true} )
+   
+    response.render('index.ejs', { items: todoItems, left: itemsLeft, completed: itemscompleted })
     // db.collection('todos').find().toArray()
     // .then(data => {
+        
     //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
+
+        
+    //     .then((itemsLeft) => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft, completed: itemsCompleted})
     //     })
     // })
     // .catch(error => console.error(error))
 })
 
-// Post method route
+
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false, deleted: false, pomocount: request.body.pomoValue, createdAt: Date.now})
+    
     .then(result => {
         console.log('Todo Added')
-        response.redirect('/')
+        
+         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
-//Put method route
+
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+
+    db.collection('todos').updateOne({thing: request.body.itemFromJS },{
         $set: {
-            completed: true
+            completed: true,
+            pomocount: [request.body.pomoValue, Date.now],
+
           }
     },{
         sort: {_id: -1},
@@ -61,44 +76,77 @@ app.put('/markComplete', (request, response) => {
     })
     .then(result => {
         console.log('Marked Complete')
+
         response.json('Marked Complete')
     })
     .catch(error => console.error(error))
 
 })
 
-//Put method route
+
+
+// app.put('/updateSession', (request, response) => {
+//     console.log()
+//     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+//         $set: {
+
+//             pomocount: request.body.sessionFromJS
+            
+//           }
+//     },{
+//         sort: {_id: -1},
+//         upsert: true
+//     })
+//     .then(result => {
+//         console.log('session updated ')
+//         response.json('session updated')
+//     })
+//     .catch(error => console.error(error))
+
+// })
+
+
+
 app.put('/markUnComplete', (request, response) => {
+    
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
-            completed: false
+            completed: false,
+            pomocount: request.body.pomoValue
+            
           }
     },{
         sort: {_id: -1},
-        upsert: false
+        upsert: true
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked InComplete')
+        response.json('Marked InComplete')
     })
     .catch(error => console.error(error))
 
 })
 
-// Delete method route
+// TODO: Add delete status to documents in collection
 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},
+        {
+            $set: {
+                deleted: [true, Date.now]
+                
+            }
+        })
+    
     .then(result => {
         console.log('Todo Deleted')
+        
         response.json('Todo Deleted')
     })
     .catch(error => console.error(error))
 
 })
 
-// Listening to server
-
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
-})
+}) 
